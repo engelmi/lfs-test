03f2948b1c98ad00e9007fc90644594605681701
Michael Engel <michael.engel.extern@limango.com>
2021-03-24 16:53:05 +0100

    test2

---
+++ calculator.go
@@ -0,0 +1,15 @@
+package main
+
+type Calculator interface {
+	Add(a int, b int) int
+}
+
+type calculator struct{}
+
+func (c calculator) Add(a int, b int) int {
+	return a + b
+}
+
+func NewCalculator() Calculator {
+	return &calculator{}
+}
